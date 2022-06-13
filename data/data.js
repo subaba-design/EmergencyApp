f60f17715651be28a73f9bed41f9aaf4b4079f3a
subaba <subaba199@gmail.com>
2022-06-14 06:23:26 +0900

    toireyobidashihappou done & Organize.

--- data/data.js
+++ data/data.js
@@ -8,12 +8,24 @@ const syoubyousyataiou = {
     ]
 };
 
+
+const toireyobidashihappou = {
+    name: 'トイレ呼出発報',
+    type: 'select',
+    flow: [
+        ['防災内で発報した場合', bousainaihappouToireFlow],
+        ['優先トイレ発報、現地急行の場合', yuusentoireGenchikyuukouFlow],
+        ['女子トイレ発報、現地急行の場合', joshitoireGenchikyuukouFlow]
+    ]
+};
+
+
 const emergencySelection = {
     name: '一覧',
     type: 'select',
     flow: [
         ['傷病者対応', syoubyousyataiou],
-        ['トイレ呼出発報'],
+        ['トイレ呼出発報', toireyobidashihappou],
         ['権利主張お客様対応'],
         ['EV故障発報'],
         ['EV呼出発報'],
